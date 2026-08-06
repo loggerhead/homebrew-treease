318358eef636a7e1da8982ae8d4675293b45468d
loggerhead <5056390+loggerhead@users.noreply.github.com>
2026-08-06 22:38:26 +0000

    chore: update Treease Homebrew package

--- Casks/treease.rb
+++ Casks/treease.rb
@@ -1,6 +1,6 @@
 cask "treease" do
-  version "0.2.0"
-  sha256 "REPLACED_BY_RELEASE_WORKFLOW"
+  version "0.2.4"
+  sha256 "203dcf73d5a46381defac286971898fcf74681a276b8c78f41654f12ab5f6afd"
 
   url "https://github.com/loggerhead/treease/releases/download/desktop-v#{version}/Treease_#{version}_aarch64.dmg"
   name "Treease"
